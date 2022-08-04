0faaed9d211e93fa0f328e725f6db789d565862d
hp gabriel <gabrielsbadenas@gmail.com>
2022-08-04 16:21:50 -0300

    modify index.js

--- index.js
+++ index.js
@@ -1,57 +1,14 @@
 const express = require('express'),
-router = require('./router/router'),
-options = require('./config/options'),
-app = express(),
-user={}
-//user=function(){
-  /*
-  const DonacionDAO = require('../daos/DonacionDAO.js'),
-  Mail = require('../mail/mail'),
-  efectuarDonacion = require('../casosDeUso/efectuarDonacion'),
-  listarDonaciones = require('../casosDeUso/listarDonaciones'),
-  options = require('../config/options'),
-  express = require('express'),
-  rtr = express.Router(),
-  bodyParser = require("body-parser")
-  
-  rtr.use(bodyParser.urlencoded({extended: false}))
-  rtr.use(bodyParser.json())
-  
-  var dao = new DonacionDAO(),
-  mail = new Mail(options.mail)
-  
-  rtr.get('/', function (req, res) {
-    res.send(dao.getAll())
-  })
-  
-  rtr.post('/', function(req, res) {
-    let donacion
-    try{
-      donacion = efectuarDonacion(mail, dao, req.body.monto, req.body.nombre)
-      res.status(201)
-      res.send(donacion)
-    }catch(error){
-      res.status(400)
-      res.send({errorMsg: error})
-    }
-    res.end()
-  })
-  
-  rtr.get('/:id', function (req, res) {
-    try{
-      res.send(listarDonaciones(dao.getAll(), req.params.id))
-    }catch(error){
-      res.send({errorMsg: error})
-    }
-  })
-  
-  module.exports = rtr
-  */
- //console.log('user')
-//}
-console.log(user)
+router = require('./router'),
+app = express()
+
+const options = {
+	url:{
+		port:8080,
+		ip:'localhost'
+	}
+}
 app.use('/', router)
-//app.use('user',user)
 app.listen(options.url.port, options.url.ip, () => {
   console.log(`Server running at http://${options.url.ip}:${options.url.port}/`)
 })
